c570f16c8057b8c65a5bc0f8d63c2badb59c8d3a
RamonNP <ramon.pereira3156@gmail.com>
2023-06-01 09:05:26 -0300

    função Soma adicionado.
    função Soma adicionado.

--- hello-go/main.go
+++ hello-go/main.go
@@ -1,7 +1,13 @@
 package main
 
-import "fmt"
+import (
+	"github.com/go-lang-treinamento/hello-go/hello-go/matematica"
+	"fmt"
+)
 
 func main()  {
 	fmt.Println("Hello Go");
+	resultado := matematica.Soma(1,2)
+	fmt.Printf("%v", resultado)
+	fmt.Printf("%T", resultado)
 }
---
+++ hello-go/matematica/soma.go
@@ -0,0 +1,7 @@
+package matematica
+
+var A string = "SHOW"
+
+func Soma(a int, b int) int  {
+	return a + b
+}
